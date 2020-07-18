b3743723415998e1d8caa842536db3439c68328e
Andrei Lapets <a@lapets.io>
2020-07-18 01:34:47 -0400

    Check.

--- test/Make-msvc.bat
+++ test/Make-msvc.bat
@@ -4,9 +4,12 @@
 @IF EXIST target\*.l @DEL target\*.l
 
 copy /y NUL target\test.error.invalid.l >NUL
-cl source/test.error.none.cpp /nologo /LD /Fetarget/test.error.none.l
-cl source/test.error.logic.cpp /nologo /LD /Fetarget/test.error.logic.l
-cl source/test.error.runtime.cpp /nologo /LD /Fetarget/test.error.runtime.l
+cl source/test.error.none.cpp /nologo /LD /Fetest.error.none.l
+cl source/test.error.logic.cpp /nologo /LD /Fetest.error.logic.l
+cl source/test.error.runtime.cpp /nologo /LD /Fetest.error.runtime.l
+copy "test.error.none.l" "target\test.error.none.l"
+copy "test.error.logic.l" "target\test.error.logic.l"
+copy "test.error.runtime.l" "target\test.error.runtime.l"
 
 @IF EXIST *.obj @DEL *.obj
 @IF EXIST target\*.exp @DEL target\*.exp
